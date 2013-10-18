9c616c99b457e1fd302c5d89cffa70cce872f12c
jixu <jixuxjixu@gmail.com>
2013-10-18 09:34:26 +0800

    add more add NettyBasedFederationServer

---
+++ src/main/java/com/yahoo/slingstone/demo/FederationHandler.java
@@ -0,0 +1,11 @@
+package com.yahoo.slingstone.demo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xuji
+ * Date: 10/18/13
+ * Time: 9:27 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FederationHandler {
+}
