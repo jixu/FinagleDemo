3116cbdb65275c4e1582d2c191820f38be2db57a
jixu <jixuxjixu@gmail.com>
2013-10-18 15:59:00 +0800

    add prediction server simulation

--- src/main/scala/com/yahoo/slingstone/demo/FederationHandler.scala
+++ src/main/scala/com/yahoo/slingstone/demo/FederationHandler.scala
@@ -8,18 +8,18 @@ import org.jboss.netty.handler.codec.http._
 import com.twitter.finagle.builder.ClientBuilder
 import com.twitter.finagle.redis.util.{StringToChannelBuffer, CBToString}
 import com.twitter.finagle.ChannelWriteException
+import org.jboss.netty.buffer.{ChannelBuffers, ChannelBuffer}
 
 class FederationHandler extends SimpleChannelHandler {
   implicit def s2b(str: String) = StringToChannelBuffer(str)
   override def messageReceived(ctx: ChannelHandlerContext, e: MessageEvent) = {
-    println("Got a message: " + e.getMessage)
-
     val expensiveResult: Future[String] = futurePool {
       expensiveComputation(500)
-    }
-    val asyncResult: Future[String] = doAsyncCall()
+    } flatMap { fetchPredictionResult(_) }
+    val asyncResult: Future[String] = doAsyncCall() flatMap { fetchPredictionResult(_) }
+
     val resultFuture: Future[String] = Future.join(expensiveResult, asyncResult) map { strPair =>
-      strPair._1 + " : " + strPair._2 }
+      strPair._1 + " : " + strPair._2 + "\n" }
 
     val result = Await.result(resultFuture)
     println(result)
@@ -27,6 +27,21 @@ class FederationHandler extends SimpleChannelHandler {
     val ch = ctx.getChannel
     val chFuture = ch.write(s2b(result))
     chFuture.addListener(ChannelFutureListener.CLOSE)
+  }
+
+  def fetchPredictionResult(message: String): Future[String] = {
+    val predictionReq = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/")
+    val cb: ChannelBuffer = ChannelBuffers.copiedBuffer(message)
+    predictionReq.setHeader(HttpHeaders.Names.CONTENT_LENGTH, cb.readableBytes())
+    predictionReq.setContent(cb)
+
+    predictionClient(predictionReq) flatMap { resp =>
+      Future.value(CBToString(resp.getContent))
+    } rescue {
+      case _: FailedFastException | _: ChannelWriteException | _: GlobalRequestTimeoutException =>
+        println("Fetch result from Prediction error. Using default result" + message)
+        Future.value(message)
+    }
   }
 
   override def exceptionCaught(ctx: ChannelHandlerContext, e: ExceptionEvent) = {
@@ -48,12 +63,11 @@ class FederationHandler extends SimpleChannelHandler {
 
   def expensiveComputation(timeInterval: Long): String = {
     val timeStart = System.currentTimeMillis
-    var str = "Expensive Computation"
+    var str = "expensive result"
     while (System.currentTimeMillis() - timeStart < timeInterval) {
-      // println(System.currentTimeMillis())
       1 until 10000 foreach {i => str = str.reverse}
     }
-    str
+    "expensive result"
   }
 }
 
@@ -64,6 +78,14 @@ object FederationHandler {
     .codec(http.Http())
     .hosts("127.0.0.1:8080")
     .hostConnectionLimit(100)
-    .timeout(Duration.fromMilliseconds(1))
+    .timeout(Duration.fromMilliseconds(500))
+    .build()
+
+  val predictionClient: Service[HttpRequest, HttpResponse] = ClientBuilder()
+    .name("prediction-client")
+    .codec(http.Http())
+    .hosts("127.0.0.1:9000")
+    .hostConnectionLimit(100)
+    .timeout(Duration.fromMilliseconds(500))
     .build()
 }
--- src/main/scala/com/yahoo/slingstone/demo/PredictionServer.scala
+++ src/main/scala/com/yahoo/slingstone/demo/PredictionServer.scala
@@ -2,26 +2,27 @@ package com.yahoo.slingstone.demo
 
 import com.twitter.finagle.{http, Service}
 import org.jboss.netty.handler.codec.http._
-import com.twitter.finagle.redis.util.StringToChannelBuffer
+import com.twitter.finagle.redis.util.{StringToChannelBuffer, CBToString}
 import com.twitter.util.Future
 import com.twitter.finagle.builder.ServerBuilder
 import java.net.InetSocketAddress
 
-object AsyncHttpServer extends App {
-
+object PredictionServer extends App {
   implicit def s2b(str: String) = StringToChannelBuffer(str)
 
   val service: Service[HttpRequest, HttpResponse] = new Service[HttpRequest, HttpResponse] {
     def apply(request: HttpRequest) = {
+      val message = CBToString(request.getContent)
+      Thread.sleep(500)
       val resp = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
-      resp.setContent("async result")
+      resp.setContent("Prediction(" + message + ")")
       Future.value(resp)
     }
   }
 
   val server = ServerBuilder()
     .codec(http.Http())
-    .bindTo(new InetSocketAddress(8080))
+    .bindTo(new InetSocketAddress(9000))
     .name("Async Server")
     .build(service)
 }
